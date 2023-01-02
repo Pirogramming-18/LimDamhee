895ec39661814fecfef51765dab0f0f069c6a6a6
damdami <okcall0302@gmail.com>
2023-01-03 00:25:39 +0900

    14

--- week1/1-1/p.py
+++ week1/1-1/p.py
@@ -1,4 +1,14 @@
-s = list(range(1,11))
-print(s)
-del s[-5:]
-print(s)
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if 0 <= a <= 100 and 0 <= b <= 100 and 0 <= c <= 100 and 0 <= d <= 100:
+    avg = (a+b+c+d) // 4
+
+    if avg >= 80:
+        print('합격')
+    else:
+        print('불합격')  
+else: 
+    print('잘못된 점수')
