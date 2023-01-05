ba4e7daebd0845cd60d9b28759d72b8ae52f792a
damdami <okcall0302@gmail.com>
2023-01-06 02:48:10 +0900

    35

--- 파이썬 과제/ex.py
+++ 파이썬 과제/ex.py
@@ -1,14 +1,24 @@
-class Annie:
-    def __init__(self, health, mana, ability_power):
-        self.health = health
-        self.mana = mana
-        self.ability_power = ability_power
-    
-    def tibbers(self):
-        print(f'티버: 피해량 {self.ability_power * 0.65 + 400}')
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
 
+    @classmethod
+    def from_string(cls, time_string):
+        hour, minute, second = map(int, time_string.split(':'))
+        time = cls(hour, minute, second)
+        return time
 
-health, mana, ability_power = map(float, input().split())
+    @staticmethod
+    def is_time_valid(time_string):
+        hour, minute, second = map(int, time_string.split(':'))
+        return hour <= 24 and minute <= 59 and second <= 60
 
-x = Annie(health=health, mana=mana, ability_power=ability_power)
-x.tibbers()
+time_string = input()
+
+if Time.is_time_valid(time_string):
+    t = Time.from_string(time_string)
+    print(t.hour, t.minute, t.second)
+else:
+    print('잘못된 시간 형식입니다.')
