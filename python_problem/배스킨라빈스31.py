cbb975678d88beeb44bd2d9ff170068cc999d4f7
damdami <okcall0302@gmail.com>
2023-01-06 17:38:17 +0900

    1-6

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -1,54 +1,31 @@
-#1단계
 num = 0
-
-#2단계
-cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-
-#3단계
-
-while(True):
-    if not cnt.isdigit():
-        print('정수를 입력하세요')
-        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-
-    elif int(cnt) < 1 or int(cnt) >3:
-        print('1,2,3 중 하나를 입력하세요')
-        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-    else :
-        break
-
-#4단계
-cnt = int(cnt)
 sum = 0
-
 player = 'A'
-for i in range(cnt):
-    print(f'player{player}: {i + 1}')
 
-sum += cnt
+while sum <= 31:
+    cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-#5단계
-player = 'B' if player == 'A' else 'B'
+    while(True):
+        if not cnt.isdigit():
+            print('정수를 입력하세요')
+            cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-while(True):
-    if not cnt.isdigit():
-        print('정수를 입력하세요')
-        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+        elif int(cnt) < 1 or int(cnt) >3:
+            print('1,2,3 중 하나를 입력하세요')
+            cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+        else :
+            break
 
-    elif int(cnt) < 1 or int(cnt) >3:
-        print('1,2,3 중 하나를 입력하세요')
-        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-    else :
-        break
+    cnt = int(cnt)
 
-cnt = int(cnt)
-for i in range(sum, sum + cnt):
-    print(f'player{player}: {i + 1}')
+    for i in range(sum, sum + cnt):
+        if i + 1 > 31:
+            break
+        print(f'player{player}: {i + 1}')
 
-sum += cnt
+    sum += cnt
 
-#6단계
-#7단계
-#8단계
-#9단계
+    if player == 'A':
+        player = 'B'
+    else:
+        player = 'A'
