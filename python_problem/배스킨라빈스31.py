7b75870bbf60e933dcd23c9cf37d596a9b175518
damdami <okcall0302@gmail.com>
2023-01-06 18:18:52 +0900

    1-8

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -1,39 +1,53 @@
-num = 0
-sum = 0
-player = 'A'
-flag = 0
-while sum < 31:
+def brGame():
     cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-    while(True):
+    while (True):
         if not cnt.isdigit():
             print('정수를 입력하세요')
             cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-        elif int(cnt) < 1 or int(cnt) >3:
+        elif int(cnt) < 1 or int(cnt) > 3:
             print('1,2,3 중 하나를 입력하세요')
             cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-        else :
+        else:
             break
 
-    cnt = int(cnt)
+    return cnt
 
-    for i in range(sum, sum + cnt):
+num = 0
+playerA = 0
+playerB = 0
+flag = 0
+
+while num < 31:
+    playerA = brGame()
+    playerA = int(playerA)
+
+    for i in range(num, num + playerA):
         if i + 1 > 31:
             flag = 1
             break
-        print(f'player{player}: {i + 1}')
+        print(f'playerA: {i + 1}')
 
-    if flag == 1:
+    num += playerA
+
+    if flag == 1 or num >= 31:
+        winner = 'player B'
         break
-    sum += cnt
 
-    if player == 'A':
-        player = 'B'
-    else:
-        player = 'A'
+    playerB = brGame()
+    playerB = int(playerB)
 
-if player == 'A':
-    print('player B win!')
-else:
-    print('player A win!')
+    for i in range(num, num + playerB):
+        if i + 1 > 31:
+            flag = 1
+            break
+        print(f'playerB: {i + 1}')
+
+    num += playerB
+
+    if flag == 1 or num >= 31:
+        winner = 'player A'
+        break
+
+print(f'{winner} win!')
