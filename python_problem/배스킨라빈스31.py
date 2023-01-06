6d728d677e4258104651de055a180f64ec422f90
damdami <okcall0302@gmail.com>
2023-01-06 17:28:07 +0900

    1-5

--- python_problem/배스킨라빈스31.py
+++ python_problem/배스킨라빈스31.py
@@ -25,7 +25,29 @@ player = 'A'
 for i in range(cnt):
     print(f'player{player}: {i + 1}')
 
+sum += cnt
+
 #5단계
+player = 'B' if player == 'A' else 'B'
+
+cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+while(True):
+    if not cnt.isdigit():
+        print('정수를 입력하세요')
+        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+
+    elif int(cnt) < 1 or int(cnt) >3:
+        print('1,2,3 중 하나를 입력하세요')
+        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+    else :
+        break
+
+cnt = int(cnt)
+for i in range(sum, sum + cnt):
+    print(f'player{player}: {i + 1}')
+
+sum += cnt
+
 #6단계
 #7단계
 #8단계
