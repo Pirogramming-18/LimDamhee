e4dfd96b102ea2cab05822ccc7fc4d96b1f6f0b5
damdami <okcall0302@gmail.com>
2023-01-06 15:11:57 +0900

    44

--- 파이썬 과제/ex.py
+++ 파이썬 과제/ex.py
@@ -1,15 +1,4 @@
-class NotPalindromeError(Exception):
-    def __init__(self):
-        super().__init__('회문이 아닙니다.')
+import math
 
-def palindrome(word):
-    if word != word[::-1]:
-        raise NotPalindromeError
-
-    print(word)
-
-try:
-    word = input()
-    palindrome(word)
-except NotPalindromeError as e:
-    print(e)
+r = float(input())
+print(r * r * math.pi)
