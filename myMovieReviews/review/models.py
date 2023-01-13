d3ced34b76bdb7f95802103771379e46d4bc58de
damdami <okcall0302@gmail.com>
2023-01-13 15:47:03 +0900

    year 수정, 기능 8

--- myMovieReviews/review/models.py
+++ myMovieReviews/review/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Review(models.Model):
     title = models.CharField(max_length=30)
-    #year = models.CharField(max_length=30)
+    year = models.CharField(max_length=30, default='')
     director = models.CharField(max_length=20)
     character = models.CharField(max_length=30)
     genre = models.CharField(max_length=20)
